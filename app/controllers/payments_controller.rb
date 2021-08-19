bbb6e3e67dad14f7caa85242daefb77889de8183
rubyist_yasunao <yasunao_izawa@icloud.com>
2021-08-19 14:36:41 +0900

    delete routes&actions [:edit,:update,:destory]

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,10 +16,6 @@ class PaymentsController < ApplicationController
     @payment = Payment.new
   end
 
-  # GET /payments/1/edit
-  def edit
-  end
-
   # POST /payments or /payments.json
   def create
     @payment = Payment.new(payment_params)
@@ -32,28 +28,6 @@ class PaymentsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @payment.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /payments/1 or /payments/1.json
-  def update
-    respond_to do |format|
-      if @payment.update(payment_params)
-        format.html { redirect_to @payment, notice: "Payment was successfully updated." }
-        format.json { render :show, status: :ok, location: @payment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /payments/1 or /payments/1.json
-  def destroy
-    @payment.destroy
-    respond_to do |format|
-      format.html { redirect_to payments_url, notice: "Payment was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
