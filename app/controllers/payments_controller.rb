3e3e7a2dbd4a5e7ba4bdd478c9b6c1b8a4fa20ea
rubyist_yasunao <yasunao_izawa@icloud.com>
2021-08-20 14:34:21 +0900

    inplemented view for action: :show

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
 
   # GET /payments/1 or /payments/1.json
   def show
-    @payment = params[:payment].nil? ? "Payment.find(params[:id])" : params[:payment]
+    @payment = params[:payment].nil? ? get_payment(params[:id]).body.payment : params[:payment]
     @notice=params[:notice]
   end
 
@@ -39,35 +39,41 @@ class PaymentsController < ApplicationController
   end
 
   private
-    def get_square_client
-      access_token=ENV['SQUARE_ACCESS_TOKEN']
-      location_id=ENV['LOCATION_ID']
-      case Rails.env
-      when"production"
-        environment="production"
-      else
-        environment='sandbox'
-      end
-      client = Square::Client.new(
-        access_token: access_token,
-        environment: environment
-      )
-      return client
+  def get_square_client
+    access_token=ENV['SQUARE_ACCESS_TOKEN']
+    location_id=ENV['LOCATION_ID']
+    case Rails.env
+    when"production"
+      environment="production"
+    else
+      environment='sandbox'
     end
-    def create_payment(nonce, price)
-      client=self.get_square_client
-      location_id=ENV['LOCATION_ID']
-      result = client.payments.create_payment(
-        body: {
-          source_id: nonce,
-          idempotency_key: SecureRandom.uuid(),
-          amount_money: {
-            amount: price,
-            currency: "JPY"
-          },
-          location_id: location_id
-        }
-      )
-      return result
-    end
+    client = Square::Client.new(
+      access_token: access_token,
+      environment: environment
+    )
+    return client
+  end
+  def create_payment(nonce, price)
+    client=self.get_square_client
+    location_id=ENV['LOCATION_ID']
+    result = client.payments.create_payment(
+      body: {
+        source_id: nonce,
+        idempotency_key: SecureRandom.uuid(),
+        amount_money: {
+          amount: price,
+          currency: "JPY"
+        },
+        location_id: location_id
+      }
+    )
+    return result
+  end
+  def get_payment(id)
+    client=self.get_square_client
+    result = client.payments.get_payment(
+      payment_id: id
+    )
+  end
 end
